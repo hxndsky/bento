dd560d2fc815a58d940415736da622be5468af0a
Azhrael <91879257+hxndsky@users.noreply.github.com>
2025-08-17 00:52:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '8b05d62206f459e1d298cbe5844d7d87', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
